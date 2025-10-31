08e7538f3d24f5b23ee468a5625f20c41a5cecfb
SweetRice Project <steelcal@gmail.com>
2025-10-31 12:33:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 python -m venv venv
-source ./venv/bin/activate.csh
+source ./venv/bin/activate.csh #linux 用户执行 source ./venv/bin/activate
 
 echo Enter Venv for install python packages
 pip install -r requirements.txt
